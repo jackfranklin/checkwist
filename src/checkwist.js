a24f3b1d004d81712e9f68648e0183f07347d26d
Jack Franklin <jack@jackfranklin.net>
2017-12-18 23:31:23 +0000

    You can login and stuff by clickin a button

--- src/checkwist.js
+++ src/checkwist.js
@@ -1,25 +1,43 @@
 // @flow
 import React, { Component } from 'react'
-import firebase from 'firebase'
 import { getAuthState, signUserIn, isLoggedInAnd } from './firebase/auth'
 import { writeUserData } from './firebase/db'
 
 type Props = {}
+type State = { user: ?$npm$firebase$auth$User }
 
-class Checkwist extends Component<Props> {
+class Checkwist extends Component<Props, State> {
+  state = {
+    user: undefined,
+  }
+
   componentDidMount() {
+    getAuthState(user => {
+      this.setState({ user })
+    })
+  }
+
+  login = () => {
     getAuthState(user => {
       if (!user) {
         signUserIn().then(() =>
           isLoggedInAnd(user => {
+            this.setState({ user })
             writeUserData(user.uid, user.displayName, user.photoURL)
           })
         )
       }
     })
   }
+
   render() {
-    return <p>hello</p>
+    return this.state.user ? (
+      <p>you are logged in!</p>
+    ) : (
+      <div>
+        <button onClick={this.login}>Click here to login</button>
+      </div>
+    )
   }
 }
 
