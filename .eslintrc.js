839a18ef009244536ad56221feaf7f8293e19307
Jack Franklin <jack@jackfranklin.net>
2017-12-18 22:04:53 +0000

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  extends: [
+    'unobtrusive',
+    'unobtrusive/import',
+    'unobtrusive/react',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': ['error'],
+    'react/no-unescaped-entities': 'error',
+
+    // don't think this is an issue and happens a lot
+    // with Redux connected components
+    'import/no-named-as-default': 'off',
+
+    // we want to enforce proptypes in all components
+    'react/prop-types': 'warn',
+
+    // this bug is a show stopper for us: https://github.com/yannickcr/eslint-plugin-react/issues/1389
+    'react/no-typos': 'off',
+  },
+};
